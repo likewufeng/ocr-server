124012a5cf8e6c007a8f1c7d31ea83b5a18ad3b2
wufeng <763476339@qq.com>
2026-07-10 10:53:36 +0800

    feat：优化ocr.py

--- app/api/ocr.py
+++ app/api/ocr.py
@@ -1,9 +1,9 @@
-'''
+r'''
 Author: WuFeng <763467339@qq.com>
 Date: 2026-07-08 17:15:05
 LastEditTime: 2026-07-08 20:50:29
 LastEditors: WuFeng <763467339@qq.com>
-Description: 
+Description: OCR API
 FilePath: \ocr-server\app\api\ocr.py
 Copyright 版权声明
 '''
@@ -18,18 +18,20 @@ from app.config import UPLOAD_DIR
 from app.schemas.response import ApiResponse
 from app.services.ocr_service import ocr_service
 
-from app.parsers.parser import parse
+from app.utils.layout import build_layout
+from app.parsers.parser import OCRParser
+
 
 router = APIRouter(
     prefix="",
     tags=["OCR"]
 )
 
+parser = OCRParser()
+
 
 @router.post("/ocr")
-async def ocr(
-    file: UploadFile = File(...)
-):
+async def ocr(file: UploadFile = File(...)):
 
     suffix = file.filename.split(".")[-1]
 
@@ -42,9 +44,14 @@ async def ocr(
 
     try:
 
+        # PaddleX OCR
         ocr_result = ocr_service.recognize(str(path))
 
-        document = parse(ocr_result)
+        # OCR -> Layout
+        layout = build_layout(ocr_result)
+
+        # Layout -> Document
+        document = parser.parse(layout)
 
         return ApiResponse.success(document)
 
